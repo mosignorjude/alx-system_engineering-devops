254302e87fe3cdc6e5a657287e7c2f4b8794e3f8
mosignorjude <mosignorjude2@gmail.com>
2024-07-03 16:42:26 +0100

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
-#!/usr/bin/pup
 # install flask from pip3
 
-# package resources that ensures that pip3 is installed
-package {'python3-pip':
-    ensure   => installed,
-}
-
-# exec resources to install flask via pip3
-exec { 'flask_install':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    path    => '/usr/bin',
-    require => package['python3-pip'],
+package {'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
