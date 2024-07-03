e6111ddadb6d7aca969fbd1317c399efed1f6618
mosignorjude <mosignorjude2@gmail.com>
2024-07-03 17:12:22 +0100

    install a package: fix-3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
 # install a specific version of flask (2.1.0)
-exec { 'flask':
-  command => '/usr/bin/pip3 pip3 install Flask==2.1.0',
+package { 'Flask':
+  ensure   => installed,
+  provider => 'pip3',
+  version  => '2.1.0',
 }
