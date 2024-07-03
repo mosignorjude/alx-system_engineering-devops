edafd8068834067f47007eb21dc304d8665c9dc8
mosignorjude <mosignorjude2@gmail.com>
2024-07-03 16:59:10 +0100

    install a package: fix-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# install flask from pip3
-
-package {'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+#!/usr/bin/pup
+# install an especific version of flask (2.1.0)
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
